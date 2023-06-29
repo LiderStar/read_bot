49b1d62033ff6c3a28dde06cac4156f0424139b5
Volodymir Zybin <v_admin@ukr.net>
2023-06-29 16:14:29 +0300

    Add config

--- bot.py
+++ bot.py
@@ -1,24 +1,32 @@
-
-from telebot.async_telebot import AsyncTeleBot
-import asyncio
-import os
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
-
+import os
+from config import greetings
 
 load_dotenv()
 
+TOKEN = os.environ.get('BOT_TOKEN')
 
-bot = AsyncTeleBot(os.environ.get('BOT_TOKEN')) # You can set parse_mode by default. HTML or MARKDOWN
+if TOKEN is None:
+    raise Exception('No token provided')
 
-@bot.message_handler(commands=['start', 'help'])
-async def send_welcome(message):
-	await bot.reply_to(message.chat.id, "Howdy, how are you doing?")
-	
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-async def echo_message(message):
-    await bot.reply_to(message, message.text)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=greetings)
 
 
-asyncio.run(bot.polling())
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
